ee0d001f9f57bcb1799d721e64d4c53b4316d3a3
hfigueir <hugoscp@hotmail.fr>
2018-10-30 16:46:33 +0100

    p

--- config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'user#new'
-  #post '/create', to: 'user#create', as: 'user_creation' #Pour vérifier les deux premiers formualires décommenter cette ligne
-  post '/', to: 'user#create', as: 'user_creation' #Pour vérifier le derneir formualire décommenter cette ligne
-end
